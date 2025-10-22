38b71a2e51a395183704520412949b60c3ff1df2
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-10-22 23:14:00 +0500

    local login test

--- mytest/login.spec.ts
+++ mytest/login.spec.ts
@@ -5,14 +5,15 @@ test('login test', async () => {
     const browser: Browser = await chromium.launch({ headless: false });
     const page: Page = await browser.newPage();
     await page.goto('https://practicetestautomation.com/practice-test-login/');
-    const username = page.locator('#username');
-    const password = page.locator('#password');
-    const loginButton = page.locator('#submit');
-    await username.fill('student');
-    await password.fill('Password123');
-    await loginButton.click();
-    const title = await page.title();
-    await page.screenshot({ path: 'loginpage.png' });
-    expect(title).toBe('Logged In Successfully | Practice Test Automation');
-    await browser.close();
+
+    //     const username = page.locator('#username');
+    //     const password = page.locator('#password');
+    //     const loginButton = page.locator('#submit');
+    //     await username.fill('student');
+    //     await password.fill('Password123');
+    //     await loginButton.click();
+    //     const title = await page.title();
+    //     await page.screenshot({ path: 'loginpage.png' });
+    //     expect(title).toBe('Logged In Successfully | Practice Test Automation');
+    //     await browser.close();
 })
