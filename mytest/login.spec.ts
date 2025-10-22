29d8d495a931c3d427f1b69c3fc3839100661c3c
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-10-22 18:30:52 +0500

    commit changes

---
+++ mytest/login.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect, Browser, Page } from '@playwright/test';
+import { chromium } from '@playwright/test';
+
+test('login test', async () => {
+    const browser: Browser = await chromium.launch({ headless: false });
+    const page: Page = await browser.newPage();
+    await page.goto('https://practicetestautomation.com/practice-test-login/');
+    const username = page.locator('#username');
+    const password = page.locator('#password');
+    const loginButton = page.locator('#submit');
+    await username.fill('student');
+    await password.fill('Password123');
+    await loginButton.click();
+    const title = await page.title();
+    await page.screenshot({ path: 'loginpage.png' });
+    expect(title).toBe('Logged In Successfully | Practice Test Automation');
+    await browser.close();
+})
