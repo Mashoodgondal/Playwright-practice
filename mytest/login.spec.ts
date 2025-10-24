d2a638ed4ebf73599a9862c6b6c367bae1a9886b
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-10-24 16:04:48 +0500

    my first test on my own web

--- mytest/login.spec.ts
+++ mytest/login.spec.ts
@@ -20,27 +20,36 @@
 
 
 
-import { test, expect, Browser, Page } from '@playwright/test';
+import { test, expect, Browser, Page, Locator } from '@playwright/test';
 import { chromium } from '@playwright/test';
 
-test('login test again', async () => {
+test('landing page test', async () => {
     const browser: Browser = await chromium.launch({ headless: false });
     const page: Page = await browser.newPage();
 
-    await page.goto('https://practicetestautomation.com/practice-test-login/');
+    await page.goto('http://127.0.0.1:5500/index.html');
+    const logo = page.getByTestId('logo')
+    await expect(logo).toBeVisible();
+    const heading: Locator = page.locator('h1')
+    await expect(heading).toHaveText('Acme Testing Site')
+    const subheading: Locator = page.locator("[data-testid='logo'] p")
+    await expect(subheading).toHaveText('A minimal page for automation practice')
 
-    const username = page.locator('#username');
-    const password = page.locator('#password');
-    const loginButton = page.locator('#submit');
+    // const subheading: Locator = page.locator('p.text-slate-500')
+    // await expect(subheading).toHaveText('A minimal page for automation practice ')
 
-    await username.fill('student');
-    await password.fill('Password123');
-    await loginButton.click();
+    // const username = page.locator('#username');
+    // const password = page.locator('#password');
+    // const loginButton = page.locator('#submit');
 
-    const title = await page.title();
-    await page.screenshot({ path: 'loginpage.png' });
+    // await username.fill('student');
+    // await password.fill('Password123');
+    // await loginButton.click();
 
-    expect(title).toBe('Logged In Successfully | Practice Test Automation');
+    // const title = await page.title();
+    // await page.screenshot({ path: 'loginpage.png' });
 
-    await browser.close();
+    // expect(title).toBe('Logged In Successfully | Practice Test Automation');
+
+    // await browser.close();
 });
