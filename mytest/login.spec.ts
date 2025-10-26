88d175bb5ce9b8320f11761e2498354f3a9b96e2
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-10-26 19:32:18 +0500

    do more test

--- mytest/login.spec.ts
+++ mytest/login.spec.ts
@@ -1,23 +1,3 @@
-// import { test, expect, Browser, Page } from '@playwright/test';
-// import { chromium } from '@playwright/test';
-
-// test('login test', async () => {
-//     const browser: Browser = await chromium.launch({ headless: false });
-//     const page: Page = await browser.newPage();
-//     await page.goto('https://practicetestautomation.com/practice-test-login/');
-
-//     const username = page.locator('#username');
-//     const password = page.locator('#password');
-//     const loginButton = page.locator('#submit');
-//     await username.fill('student');
-//     await password.fill('Password123');
-//     await loginButton.click();
-//     const title = await page.title();
-//     await page.screenshot({ path: 'loginpage.png' });
-//     expect(title).toBe('Logged In Successfully | Practice Test Automation');
-//     //     await browser.close();
-// })
-
 
 
 import { test, expect, Browser, Page, Locator } from '@playwright/test';
@@ -35,39 +15,7 @@ test('landing page test', async () => {
     const subheading: Locator = page.locator("[data-testid='logo'] p")
     await expect(subheading).toHaveText('A minimal page for automation practice')
 
-    // const subheading: Locator = page.locator('p.text-slate-500')
-    // await expect(subheading).toHaveText('A minimal page for automation practice ')
 
-
-    // const username = page.locator('#username');
-    // const password = page.locator('#password');
-    // const loginButton = page.locator('#submit');
-
-    // await username.fill('student');
-    // await password.fill('Password123');
-    // await loginButton.click();
-
-    // const title = await page.title();
-    // await page.screenshot({ path: 'loginpage.png' });
-
-    // expect(title).toBe('Logged In Successfully | Practice Test Automation');
-
-    // await browser.close();
-
-    // const username = page.locator('#username');
-    // const password = page.locator('#password');
-    // const loginButton = page.locator('#submit');
-
-    // await username.fill('student');
-    // await password.fill('Password123');
-    // await loginButton.click();
-
-    // const title = await page.title();
-    // await page.screenshot({ path: 'loginpage.png' });
-
-    // expect(title).toBe('Logged In Successfully | Practice Test Automation');
-
-    // await browser.close();
 });
 
 test('form check', async () => {
