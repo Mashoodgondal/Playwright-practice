d48d550cb7d201220e5c19ed8fb435f1d7918688
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-10-27 17:33:04 +0500

    add radio button test

--- mytest/login.spec.ts
+++ mytest/login.spec.ts
@@ -29,7 +29,7 @@ test('form check', async () => {
 test('form test', async () => {
     const browser: Browser = await chromium.launch({ headless: false });
     const page: Page = await browser.newPage()
-    await page.goto('http://127.0.0.1:5500/index.html')
+    await page.goto('http://127.0.0.1:5500/index.html#')
     const form: Locator = page.locator('.mt-6.grid.gap-4')
     await expect(form).toBeVisible()
     const nameInput: Locator = page.locator('input[name="name"]')
@@ -41,5 +41,17 @@ test('form test', async () => {
     const passwordInput: Locator = page.locator('input[name="password"]')
     await passwordInput.fill('ali321')
     await expect(passwordInput).toHaveValue('ali321')
+    const checkbox: Locator = page.locator('input[type="checkbox"]')
+    await checkbox.check()
+    await expect(checkbox).toBeChecked()
+    const fieldSet: Locator = page.locator('fieldset.mt-2')
+    await expect(fieldSet).toBeVisible()
+    const radiofree: Locator = page.locator('fieldset.mt-2.mt-2.flex.gap-4 label:first-child ')
+    const radiopro: Locator = page.locator('fieldset.mt-2.mt-2.flex.gap-4 label:last-child')
+    await expect(radiofree).toBeChecked()
+    await expect(radiopro).not.toBeChecked()
+    await radiopro.check()
+    await expect(radiopro).toBeChecked()
+    await expect(radiofree).not.toBeChecked()
 
 })
