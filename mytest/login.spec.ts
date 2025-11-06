30e4364317c9188bf1408596e43fec98df8bd30e
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-11-06 21:26:39 +0500

    one test more

--- mytest/login.spec.ts
+++ mytest/login.spec.ts
@@ -1,22 +1,5 @@
 
 
-//  import { test, expect, Browser, Page, Locator } from '@playwright/test';
-// import { chromium } from '@playwright/test';
-
-// test('landing page test', async () => {
-//     const browser: Browser = await chromium.launch({ headless: false });
-//     const page: Page = await browser.newPage();
-
-//     await page.goto('http://127.0.0.1:5500/index.html');
-//     const logo = page.getByTestId('logo')
-//     await expect(logo).toBeVisible();
-//     const heading: Locator = page.locator('h1')
-//     await expect(heading).toHaveText('Acme Testing Site')
-//     const subheading: Locator = page.locator("[data-testid='logo'] p")
-//     await expect(subheading).toHaveText('A minimal page for automation practice')
-// });
-
-
 
 test('form check', async () => {
     const browser: Browser = await chromium.launch({ headless: false });
