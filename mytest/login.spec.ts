16b9aade81d8e860ebb09aa24c8c06e96f154526
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-11-08 13:17:28 +0500

    ...

--- mytest/login.spec.ts
+++ mytest/login.spec.ts
@@ -23,6 +23,16 @@
 // })
 
 
+// test('form check', async () => {
+//     const browser: Browser = await chromium.launch({ headless: false });
+//     const page: Page = await browser.newPage()
+//     await page.goto('http://127.0.0.1:5500/index.html')
+//     const heading: Locator = page.locator('#form-heading')
+//     await expect(heading).toHaveText('User sign-up form')
+
+// })
+
+
 
 
 
