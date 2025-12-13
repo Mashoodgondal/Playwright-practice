7beceeb5a5791e5f73fe48916a7661738f389dee
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-12-13 10:49:14 +0500

    update previous test

--- mytest/login.spec.ts
+++ mytest/login.spec.ts
@@ -12,6 +12,19 @@
 // });
 // import { test, expect, chromium, Browser, Page, Locator } from '@playwright/test';
 
+// let browser: Browser;
+// let page: Page;
+
+// test.beforeEach(async () => {
+//     browser = await chromium.launch({ headless: false });
+//     page = await browser.newPage();
+
+
+//     await page.goto('http://127.0.0.1:5500/index.html');
+// });
+// import { test, expect, chromium, Browser, Page, Locator } from '@playwright/test';
+
+
 
 
 
