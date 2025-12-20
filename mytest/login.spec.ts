018c768b0809e9d06c96509dcb89dc0736c5fe4d
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-12-20 10:45:00 +0500

    simple test

--- mytest/login.spec.ts
+++ mytest/login.spec.ts
@@ -22,6 +22,17 @@
 // let browser: Browser;
 // let page: Page;
 
+// let browser: Browser;
+// let page: Page;
+
+// test.beforeEach(async () => {
+//     browser = await chromium.launch({ headless: false });
+//     page = await browser.newPage();
+
+
+// let browser: Browser;
+// let page: Page;
+
 
 
 import { test, expect, chromium, Browser, Page, Locator } from '@playwright/test';
